4890c087b1e330d677ff10b1a51f396e7a960585
taccisum <liaojinfeng@deepexi.com>
2021-12-24 13:20:00 +0800

    test: add simple controller test

---
+++ src/test/java/com/github/taccisum/ol/controller/VersionControllerTest.java
@@ -0,0 +1,35 @@
+package com.github.taccisum.ol.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * @author taccisum - liaojinfeng6938@dingtalk.com
+ * @since 2021/12/24
+ */
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class VersionControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void versions() throws Exception {
+        mvc.perform(get("/versions")
+                        .accept(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.content().string("1.0-SNAPSHOT"));
+    }
+}
