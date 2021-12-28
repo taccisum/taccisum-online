dfd4745dcc389ac5bca63300c08b40f0847fba11
taccisum <liaojinfeng@deepexi.com>
2021-12-28 15:34:42 +0800

    chore: integration swagger & devtools

--- src/main/java/com/github/taccisum/ol/controller/VersionController.java
+++ src/main/java/com/github/taccisum/ol/controller/VersionController.java
@@ -1,5 +1,7 @@
 package com.github.taccisum.ol.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author taccisum - liaojinfeng6938@dingtalk.com
  * @since 2021/12/24
  */
+@Api
 @RestController
 @RequestMapping("versions")
 public class VersionController {
@@ -16,6 +19,7 @@ public class VersionController {
     private String version;
 
     @GetMapping
+    @ApiOperation("获取服务版本号")
     public String current() {
         return version;
     }
