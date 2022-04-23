2aa1f3d5dfff4063171572630df211ce48ca4c1b
taccisum <514162920@qq.com>
2022-04-23 12:10:06 +0800

    refactor: do not crash app when SendNoticeListerner throw exception

--- src/main/java/com/github/taccisum/ol/listener/SendNoticeListener.java
+++ src/main/java/com/github/taccisum/ol/listener/SendNoticeListener.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+import lombok.extern.slf4j.Slf4j;
+
 import javax.annotation.PreDestroy;
 import javax.annotation.Resource;
 
@@ -16,6 +18,7 @@ import javax.annotation.Resource;
  * @author taccisum - liaojinfeng6938@dingtalk.com
  * @since 2022/2/28
  */
+@Slf4j
 @Component
 @Profile("prod")
 public class SendNoticeListener {
@@ -24,22 +27,40 @@ public class SendNoticeListener {
 
     @EventListener(ApplicationStartedEvent.class)
     public void onStarted(ApplicationStartedEvent e) {
-        serviceProviderRepo.<Pigeon>getAndCast(ServiceProvider.Type.PIGEON)
-                .getAccount()
-                .sendTemplateMessage(7, "514162920@qq.com", "robot_01@smtp.66cn.top");
+        handErr(() -> {
+            serviceProviderRepo.<Pigeon>getAndCast(ServiceProvider.Type.PIGEON)
+                    .getAccount()
+                    .sendTemplateMessage(7, "514162920@qq.com", "robot_01@smtp.66cn.top");
+        });
     }
 
     @EventListener(ApplicationFailedEvent.class)
     public void onFailed(ApplicationFailedEvent e) {
-        serviceProviderRepo.<Pigeon>getAndCast(ServiceProvider.Type.PIGEON)
-                .getAccount()
-                .sendTemplateMessage(8, "514162920@qq.com", "robot_01@smtp.66cn.top");
+        handErr(() -> {
+            serviceProviderRepo.<Pigeon>getAndCast(ServiceProvider.Type.PIGEON)
+                    .getAccount()
+                    .sendTemplateMessage(8, "514162920@qq.com", "robot_01@smtp.66cn.top");
+        });
     }
 
     @PreDestroy
     public void onExit() {
-        serviceProviderRepo.<Pigeon>getAndCast(ServiceProvider.Type.PIGEON)
-                .getAccount()
-                .sendTemplateMessage(9, "514162920@qq.com", "robot_01@smtp.66cn.top");
+        handErr(() -> {
+            serviceProviderRepo.<Pigeon>getAndCast(ServiceProvider.Type.PIGEON)
+                    .getAccount()
+                    .sendTemplateMessage(9, "514162920@qq.com", "robot_01@smtp.66cn.top");
+        });
+    }
+
+    void handErr(Action act) {
+        try {
+            act.doAct();
+        } catch (Exception e) {
+            log.warn("发送系统事件相关通知失败", e);
+        }
+    }
+
+    interface Action {
+        void doAct();
     }
 }
